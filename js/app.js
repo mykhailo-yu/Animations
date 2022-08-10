648c7db1b582844d539ebb58e2ce600563b409f6
Mykhailo <mishayurkiv15@gmail.com>
2022-08-10 23:08:07 +0300

    finished eyes

--- js/app.js
+++ js/app.js
@@ -21,36 +21,39 @@ let clientWidth = document.documentElement.clientWidth
 let clientHeight = document.documentElement.clientHeight
 let coefX = [], coefY = []
 let pupilsDisplacementX = [], pupilsDisplacementY = []
-for (let i = 0; i < eyeBg.length; i++) {
-    eyeBgWidth[i] = Math.round(eyeBg[i].getBoundingClientRect().width)
-    eyeBgHeight[i] = Math.round(eyeBg[i].getBoundingClientRect().height)
-}
-for (let i = 0; i < pupils.length; i++) {
-    pupilsWidth[i] = Math.round(pupils[i].getBoundingClientRect().width)
-    pupilsHeight[i] = Math.round(pupils[i].getBoundingClientRect().height)
-}
-for (let i = 0; i < eyeBg.length; i++) {
-    lengthDifferenceWidth[i] = eyeBgWidth[i] - pupilsWidth[i]
-    lengthDifferenceWidth[i] = lengthDifferenceWidth[i] - lengthDifferenceWidth[i] / 2
-    lengthDifferenceHeight[i] = eyeBgHeight[i] - pupilsHeight[i]
-    lengthDifferenceHeight[i] = lengthDifferenceHeight[i] - lengthDifferenceHeight[i] / 2
-}
-for (let i = 0; i < pupils.length; i++) {
-    pupilsX[i] = Math.round(pupils[i].getBoundingClientRect().left) + Math.round(pupilsWidth[i] / 2)
-    pupilsY[i] = Math.round(pupils[i].getBoundingClientRect().top) + Math.round(pupilsHeight[i] / 2)
-}
-for (let i = 0; i < pupils.length; i++) {
-    coefX[i] = lengthDifferenceWidth[i] / clientWidth
-    coefY[i] = lengthDifferenceHeight[i] / clientHeight
-}
-document.onmousemove = function (event) {
-    let x = event.clientX - 500
-    let y = event.clientY
+function eyeMove() {
+    for (let i = 0; i < eyeBg.length; i++) {
+        eyeBgWidth[i] = Math.round(eyeBg[i].getBoundingClientRect().width)
+        eyeBgHeight[i] = Math.round(eyeBg[i].getBoundingClientRect().height)
+    }
     for (let i = 0; i < pupils.length; i++) {
-        pupilsDisplacementX[i] = x * coefX[i]
-        pupilsDisplacementY[i] = y * coefY[i]
-        eyeBg[i].style.transform = "translate(" + pupilsDisplacementX[i] + "px," + pupilsDisplacementY[i] + "px  )";
-        pupils[i].style.transform = "translate(" + (1.5 * pupilsDisplacementX[i]) + "px," + (1.5 * pupilsDisplacementY[i]) + "px  )";
-        pupils1[i].style.transform = "translate(" + (2 * pupilsDisplacementX[i]) + "px," + (2 * pupilsDisplacementY[i]) + "px  )";
+        pupilsWidth[i] = Math.round(pupils[i].getBoundingClientRect().width)
+        pupilsHeight[i] = Math.round(pupils[i].getBoundingClientRect().height)
+    }
+    for (let i = 0; i < eyeBg.length; i++) {
+        lengthDifferenceWidth[i] = eyeBgWidth[i] - pupilsWidth[i]
+        lengthDifferenceWidth[i] = lengthDifferenceWidth[i] - lengthDifferenceWidth[i] / 2
+        lengthDifferenceHeight[i] = eyeBgHeight[i] - pupilsHeight[i]
+        lengthDifferenceHeight[i] = lengthDifferenceHeight[i] - lengthDifferenceHeight[i] / 2
+    }
+    for (let i = 0; i < pupils.length; i++) {
+        pupilsX[i] = Math.round(pupils[i].getBoundingClientRect().left) + Math.round(pupilsWidth[i] / 2)
+        pupilsY[i] = Math.round(pupils[i].getBoundingClientRect().top) + Math.round(pupilsHeight[i] / 2)
+    }
+    for (let i = 0; i < pupils.length; i++) {
+        coefX[i] = lengthDifferenceWidth[i] / clientWidth
+        coefY[i] = lengthDifferenceHeight[i] / clientHeight
+    }
+    document.onmousemove = function (event) {
+        let x = event.clientX - 500
+        let y = event.clientY
+        for (let i = 0; i < pupils.length; i++) {
+            pupilsDisplacementX[i] = x * coefX[i]
+            pupilsDisplacementY[i] = y * coefY[i]
+            eyeBg[i].style.transform = "translate(" + pupilsDisplacementX[i] + "px," + pupilsDisplacementY[i] + "px  )";
+            pupils[i].style.transform = "translate(" + (1.5 * pupilsDisplacementX[i]) + "px," + (1.5 * pupilsDisplacementY[i]) + "px  )";
+            pupils1[i].style.transform = "translate(" + (2 * pupilsDisplacementX[i]) + "px," + (2 * pupilsDisplacementY[i]) + "px  )";
+        }
     }
 }
+setTimeout(eyeMove, 1000)
